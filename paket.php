907a9e4dd874cb83f7fe2c27ee4fe01c47ca693b
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-01-07 23:04:07 +0700

    paket

--- paket.php
+++ paket.php
@@ -1,45 +1,26 @@
-<?php 
+<?php
 
 include 'class/paket.php';
 
-$db= new paket();
+$db = new paket();
 
 
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
+
+<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>WAADADAD</title>
+	<title>LUQEFOTO</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
 	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
 	<meta name="author" content="FREEHTML5.CO" />
-
-  <!-- 
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-		
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
+	<meta property="og:title" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:description" content="" />
 	<meta name="twitter:title" content="" />
 	<meta name="twitter:image" content="" />
 	<meta name="twitter:url" content="" />
@@ -47,9 +28,7 @@ $db= new paket();
 
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<link rel="shortcut icon" href="favicon.ico">
-
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
-	
 	<!-- Animate.css -->
 	<link rel="stylesheet" href="css/animate.css">
 	<!-- Icomoon Icon Fonts-->
@@ -65,74 +44,57 @@ $db= new paket();
 	<!-- CS Select -->
 	<link rel="stylesheet" href="css/cs-select.css">
 	<link rel="stylesheet" href="css/cs-skin-border.css">
-	
 	<link rel="stylesheet" href="css/style.css">
-
-
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+</head>
 
-	<style type="text/css">
-<!--
-.style1 {color: #FF5F00}
--->
-    </style>
-	</head>
-	<body>
-		<div id="fh5co-wrapper">
+<body>
+	<div id="fh5co-wrapper">
 		<div id="fh5co-page">
-
 			<?php include("header.php") ?>
+			<!-- end:header-top -->
 
-		<!-- end:header-top -->
-		
-		<div id="fh5co-tours" class="fh5co-section-gray">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-						<h3>Daftar Paket Foto di WeADADADA</h3>
-						<p>Paket hemat untuk pemotretan bersama orang terkasih</p>
-					</div>
-				</div>
-				<div class="row">
-						<?php
-	$no = 1;
-	foreach($db->tampil_data() as $x){
-	?>
-					<div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">
-						<div href="#"><img src="images/fotografer.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-							<div class="desc">
-								<span></span>
-								<h3><?php echo $x['nama_paket']; ?></h3>
-								<span><?php echo $x['ket_paket']; ?></span>
-								<span class="price"><?php echo $x['harga_paket']; ?></span>
-								
-							</div>
+			<div id="fh5co-tours" class="fh5co-section-gray">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+							<h3>Daftar Paket Foto di LUQEFOTO</h3>
+							<p>Paket hemat untuk pemotretan bersama orang terkasih</p>
 						</div>
-
 					</div>
+					<div class="row">
+						<?php
+						function rupiah($angka)
+						{
+							$hasil_rupiah = "Rp. " . number_format($angka, 0, ',', '.');
+							return $hasil_rupiah;
+						}
+						foreach ($db->tampil_data() as $x) {
+						?>
+							<div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">
+								<div href="#"><img src="images/fotografer.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+									<div class="desc">
+										<span></span>
+										<h3><?php echo $x['nama_paket']; ?></h3>
+										<span><?php echo $x['ket_paket']; ?></span>
+										<span class="price"><?php echo rupiah($x['harga_paket']); ?></span>
+									</div>
+								</div>
+							</div>
 						<?php } ?>
-					<div class="col-md-12 text-center animate-box">
-						<p>
-							
-						</p>
 					</div>
 				</div>
 			</div>
+
+
+			<!-- fh5co-blog-section -->
+
+			<?php include("footer.php") ?>
+
+
 		</div>
-
-		
-		<!-- fh5co-blog-section -->
-		
-		<?php include("footer.php") ?>
-	
-
-	</div>
-	<!-- END fh5co-page -->
+		<!-- END fh5co-page -->
 
 	</div>
 	<!-- END fh5co-wrapper -->
@@ -162,10 +124,10 @@ $db= new paket();
 	<!-- CS Select -->
 	<script src="js/classie.js"></script>
 	<script src="js/selectFx.js"></script>
-	
+
 	<!-- Main JS -->
 	<script src="js/main.js"></script>
 
-	</body>
-</html>
+</body>
 
+</html>
