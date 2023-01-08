5a79a03a169388ef08bed4d5e57aebfb9b3524a5
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-01-08 11:15:25 +0700

    style login admin

--- Admin/index.php
+++ Admin/index.php
@@ -1,25 +1,17 @@
      <?php
-
 			session_start();
 			if ($_SESSION) {
 				header("Location: operator.php");
 			} elseif ($_SESSION) {
 				header("Location: admin.php");
 			}
-
 			include '../class/admin.php';
 			$admin = new admin();
-
-
 			if (isset($_SESSION['Login'])) {
 				header("location:index.php");
 			}
-
 			if (isset($_POST['Login'])) {
-
 				$login = $admin->login_admin($_POST['username'], $_POST['password']);
-
-
 				if ($login) {
 					if ($_SESSION['level'] == "1") {
 						header("location: admin.php");
@@ -50,20 +42,12 @@
      	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
      	<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
      	<link href="css/style.css" rel="stylesheet">
-     	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-     	<!--[if lt IE 9]>
-				<script src="http://getbootstrap.com/docs-assets/js/html5shiv.js"></script>
-				<script src="http://getbootstrap.com/docs-assets/js/respond.min.js"></script>
-		<![endif]-->
      </head>
 
      <body>
      	<div class="container-fluid">
      		<div id="page-login" class="row">
-     			<div class="col-xs-12 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-     				<div class="text-right">
-
-     				</div>
+     			<div class="form-login" style="width: 300px;">
      				<div class="box">
      					<div class="box-content">
      						<div class="text-center">
@@ -72,20 +56,18 @@
      						<form role="form" action="" method="post">
      							<div class="form-group">
      								<label class="control-label">Username</label>
-     								<input type="text" class="form-control" name="username" />
+     								<input type="text" class="form-control" name="username" autofocus required />
      							</div>
      							<div class="form-group">
      								<label class="control-label">Password</label>
-     								<input type="password" class="form-control" name="password" />
+     								<input type="password" class="form-control" name="password" required />
      							</div>
-
      							<input type="submit" name="Login" class="btn btn-primary btn-block" value="Log in" />
+     						</form>
      					</div>
      				</div>
      			</div>
-     			</form>
      		</div>
-     	</div>
      	</div>
      </body>
 
